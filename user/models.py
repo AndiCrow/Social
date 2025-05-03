c53746ed9474c7d50e4ea69c31b2dce84b8ad98d
Andreas Rajchert <andi14-rajchert@web.de>
2025-05-03 23:00:48 +0200

    urls fix and login

--- user/models.py
+++ user/models.py
@@ -1,8 +1,21 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 class CustomUser(AbstractUser):
-    pass
+    groups = models.ManyToManyField(
+        Group,
+        related_name="customuser_groups",  # Eindeutiger related_name
+        blank=True,
+        help_text="The groups this user belongs to.",
+        verbose_name="groups",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="customuser_permissions",  # Eindeutiger related_name
+        blank=True,
+        help_text="Specific permissions for this user.",
+        verbose_name="user permissions",
+    )
 
 class Profile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
